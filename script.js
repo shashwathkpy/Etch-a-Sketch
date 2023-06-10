01f2da2366293d987761f8905ef98891ccaafcd3
shashwathkpy <shashwathkpy@gmail.com>
2023-06-10 12:00:16 -0700

    grid size can be changed, and boxes still fit to new grid

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const container = document.querySelector("#container");
 const gridBtn = document.querySelector("#gridBtn");
 let gridSize = 16;
 
+const containerSize = 800;
+const containerSizeString = "width: " + containerSize + "px; height: " + containerSize + "px";
+container.setAttribute('style', containerSizeString);
+
 grid();
 
 gridBtn.addEventListener('click', function(e)
@@ -20,7 +24,12 @@ function grid()
     {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
-        let customId = 'b' + i.toString();
+
+        let boxSize = containerSize/gridSize;
+        let boxSizeString = "width: " + boxSize + "px; height: " + boxSize + "px";
+        box.setAttribute('style', boxSizeString);
+
+        let customId = 'b' + i;
         box.setAttribute('id', customId);
         container.appendChild(box);
     
